da6938197f47a59c942982777d3116a4f61bc96a
Gift Ayodele <gift.ayodele@reventtechnologies.com>
2022-07-13 13:12:48 +0100

    Add project files.

---
+++ DapperDemo/Repository/CompanyRepositoryDapper.cs
@@ -0,0 +1,55 @@
+﻿using Dapper;
+using DapperDemo.Data;
+using DapperDemo.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace DapperDemo.Repository
+{
+    public class CompanyRepositoryDapper : ICompanyRepository
+    {
+        private IDbConnection dbConnection;
+
+        public CompanyRepositoryDapper(IConfiguration configuration)
+        {
+           this.dbConnection = new SqlConnection(configuration.GetConnectionString("DefaultConnection"));
+        }
+        public Company Add(Company company)
+        {
+            var sql = "INSERT INTO Companies (Name, Address, City, State, PostalCode) VALUES (@Name, @Address, @City, @State, @PostalCode);"
+            + "SELECT CAST(SCOPE_IDENTITY() as int); ";
+            var id =dbConnection.Query<int>(sql,company).Single();
+            company.CompanyId = id;
+            return company;
+        }
+
+        public Company Find(int id)
+        {
+            var sql = "SELECT * FROM Companies WHERE CompanyId = @CompanyId";
+            return dbConnection.Query<Company>(sql, new { @CompanyId = id }).Single();
+        }
+
+        public List<Company> GetAll()
+        {
+            var sql = "SELECT * FROM Companies";
+            return dbConnection.Query<Company>(sql).ToList();
+        }
+
+        public void Remove(int id)
+        {
+            var sql = "DELETE FROM Companies WHERE CompanyId = @Id";
+            dbConnection.Execute(sql, new { id });
+        }
+
+        public Company Update(Company company)
+        {
+            var sql = "UPDATE Companies SET Name = @Name, Address = @Address , City = @City, " +
+                "State = @State, PostalCode = @PostalCode WHERE CompanyId = @CompanyId";
+            dbConnection.Execute(sql, company);
+            return company;
+        }
+    }
+}
---
+++ DapperDemo/Repository/CompanyRepositoryEF.cs
@@ -0,0 +1,49 @@
+﻿using DapperDemo.Data;
+using DapperDemo.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DapperDemo.Repository
+{
+    public class CompanyRepositoryEF : ICompanyRepository
+    {
+        private readonly ApplicationDbContext _dbcontext;
+
+        public CompanyRepositoryEF(ApplicationDbContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+
+        public Company Add(Company company)
+        {
+            _dbcontext.Add(company);
+            _dbcontext.SaveChanges();
+            return company;
+        }
+
+        public Company Find(int id)
+        {
+            return _dbcontext.Companies.FirstOrDefault(c => c.CompanyId == id);
+        }
+
+        public List<Company> GetAll()
+        {
+            return _dbcontext.Companies.ToList();
+        }
+
+        public void Remove(int id)
+        {
+         Company company  = _dbcontext.Companies.FirstOrDefault(c => c.CompanyId == id);
+            _dbcontext.Companies.Remove(company);
+            _dbcontext.SaveChanges();
+            return;
+        }
+
+        public Company Update(Company company)
+        {
+            _dbcontext.Update(company);
+            _dbcontext.SaveChanges();
+            return company;
+        }
+    }
+}
